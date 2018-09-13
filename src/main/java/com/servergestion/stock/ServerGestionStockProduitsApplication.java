f1bf6a103681a72110cb55a2f3bd0ba0e11562b1
Oumaruto Keitaki <keimar.ok@gmail.com>
2018-09-13 16:13:32 +0100

    First Commit

---
+++ src/main/java/com/servergestion/stock/ServerGestionStockProduitsApplication.java
@@ -0,0 +1,12 @@
+package com.servergestion.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerGestionStockProduitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerGestionStockProduitsApplication.class, args);
+	}
+}
